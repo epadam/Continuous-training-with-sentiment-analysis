75e59ab9b2d2d694161eb570e6491bd78a53ff21
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2022-03-22 05:01:22 +0100

    Update utils.py

--- train_pipeline/utils.py
+++ train_pipeline/utils.py
@@ -5,8 +5,16 @@ from typing import List, Text
 import tensorflow_hub as hub
 import tensorflow_transform as tft
 
+from tfx.components.trainer.fn_args_utils import FnArgs
+from tfx.components.tuner.component import TunerFnResult
+
 # Define dataset
 Class Dataset
 
 # Define model 
 Class Model 
+
+
+# train class
+
+Class
