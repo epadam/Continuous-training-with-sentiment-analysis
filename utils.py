4336712945871c52dbfc64ac3748cd4c505db054
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2022-03-22 02:40:59 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+from typing import List, Text
+
+import tensorflow_hub as hub
+import tensorflow_transform as tft
+
+# Define dataset
+Class Dataset
+
+# Define model 
+Class Model 
